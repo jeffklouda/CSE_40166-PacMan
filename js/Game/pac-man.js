4b39b6ed72f5b3d07b04319eb660504645853d8e
Pedro Saunero <psaunero@nd.edu>
2017-12-07 18:22:35 -0500

    More stuff

--- js/Game/pac-man.js
+++ js/Game/pac-man.js
@@ -74,7 +74,25 @@ class Ghost {
     this.direction = Direction.LEFT;
   }
 
-  move(){
+  move(game){
+    if(game.map.hasIntersection(this.position)){
+      this.handleIntersection(game);
+    }else if(game.map.hasTwoWayIntersection(this.position)){
+      this.handleTwoWayIntersection(game);
+    }else{
+      this.normalMove(game);
+    }
+  }
+
+  normalMove(game){
+  }
+
+  handleTwoWayIntersection(game){
+
+  }
+
+  handleIntersection(game){
+
   }
 
   switchDirection(){
@@ -123,38 +141,110 @@ class Map {
       case 5: return new Tile(TileType.BOX, TileContents.EMPTY);
     }
   }
+
+  hasWall(x, y){
+    return this.board[x][y].type == TileType.WALL || this.board[x][y].type == TileType.BOX;
+  }
+
+  hasTwoWayIntersection(position){
+    return true;
+  }
+
+  hasIntersection(position){
+    return this.board[position.x][position.y].type == TileType.INTERSECTION;
+  }
 };
 
 class PacMan {
   constructor(position){
     this.position = position;
+    this.startPosition = position;
     this.direction = Direction.RIGHT;
     this.lives = 1;
+    this.alive = true;
   }
 
-  move(board){
+  move(game){
+    var map = game.map;
+    switch(this.direction){
+      case Direction.UP:
+        if(!map.hasWall(this.position.x - 1, this.position.y))
+          this.position.x -= 1;
+        break;
+      case Direction.DOWN:
+        if(!map.hasWall(this.position.x + 1, this.position.y))
+          this.position.x += 1;
+        break;
+      case Direction.RIGHT:
+        if(!map.hasWall(this.position.x, this.position.y + 1))
+          this.position.y += 1;
+        break;
+      case Direction.LEFT:
+        if(!map.hasWall(this.position.x, this.position.y - 1))
+          this.position.y -= 1;
+        break;
+    }
+    this.checkTile(game);
   }
+
+  checkTile(game){
+    if(game.ghostInPosition(this.position)){
+      this.loseLife();
+      return;
+    }
+
+    game.updateTile(this.position);
+  }
+
 }
 
 class Game {
   constructor(prototype){
     this.map = new Map(prototype);
-    this.pacman = new Pacman(new Position(12, 23));
+    this.pacman = new PacMan(new Position(12, 23));
     this.ghosts = [new Ghost(new Position(13, 11))];
     this.moveDirection = Direction.RIGHT;
     this.score = 0;
-
   }
 
   update(){
-    this.pacman.move(this.map.board);
+    this.pacman.move(this);
     this.ghosts.forEach(function(ghost){
-      ghost.move(this.map.board);
+      ghost.move(this);
     }.bind(this));
   }
+
+  setDirection(direction){
+    this.moveDirection = direction;
+    this.pacman.direction = direction;
+  }
+
+  pacManInPosition(position){
+    return ((this.pacman.position.x == position.x)
+      && (this.pacman.position.y == position.y));
+  }
+
+  ghostInPosition(position){
+    for(let ghost of this.ghosts){
+      if((ghost.position.x == position.x) && (ghost.position.y == position.y)){
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  updateTile(position){
+    if(this.map.board[position.x][position.y].content == TileContents.DOT){
+      this.map.board[position.x][position.y].content = TileContents.EMPTY;
+      game.score += 1;
+    }
+  }
+
 }
 
 var myMap = new Map(boardPrototype);
-console.log(myMap.board);
-console.log(myMap.height);
-console.log(myMap.width);
+var game = new Game(boardPrototype);
+console.log(game.pacman.position);
+game.update();
+console.log(game.pacman.position);
